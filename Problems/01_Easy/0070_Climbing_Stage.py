5b091cca073684a431f5c15357151485855ab7ce
ChunYenDu <chun.yen.tu@gmail.com>
2019-11-20 00:19:07 +0800

    2019-11-19 More about easy 0070 Climbe stages

--- Problems/01_Easy/0070_Climbing_Stage.py
+++ Problems/01_Easy/0070_Climbing_Stage.py
@@ -1,4 +1,41 @@
-class Solution:
+class Solution1:
+    '''
+    記憶化搜尋：把歷遍的答案記下避免重複計算產生超時問題
+    '''
+
+    def __init__(self):
+        self.memo = dict()  # create a empty dictiony to save result
+        self.memo[0] = 1  # stage = 0  step = 1
+        self.memo[1] = 1  # stage = 1  step = 1
+
+    def climbStairs(self, n: int) -> int:
+        if n in self.memo:
+            return self.memo[n]
+        steps = self.climbStairs(n-1)+self.climbStairs(n-2)
+        self.memo[n] = steps
+        return steps
+
+
+class Solution2:
+    '''
+    動態規劃: 建立一個數組, 從頭開始歷遍, 這題的答案是前2個數相加, 看最後一個數值就好
+
+    '''
+
+    def ClimbStairs(self, n):
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[-1]
+
+
+class Solution3:
+    '''
+    空間壓縮： 觀察可得所由解答可由 stage = 1 跟 stage = 2 取得進行改寫
+    '''
+
     def climbStairs(self, n: int) -> int:
         if n <= 1:
             return 1
@@ -9,4 +46,4 @@ class Solution:
             temp = cur   # f(n-1)
             cur = cur + prev  # calcuate f(n)
             prev = temp   # f(n-1) will become f(n-2) at the next iteration
-        return cur  
+        return cur
